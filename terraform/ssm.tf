385876c372e99b3225fcaa512d7ff7a82077d7c8
Jamesson Faria <jamesson.da.s.junior@accenture.com>
2022-04-27 14:24:25 -0300

    Adjust name to ssm parameter

--- terraform/ssm.tf
+++ terraform/ssm.tf
@@ -1,4 +1,4 @@
-resource "aws_ssm_parameter" "table" {
+resource "aws_ssm_parameter" "dynamodb_table" {
   name  = "${local.namespaced_service_name}-dynamodb-table"
   type  = "String"
   value = aws_dynamodb_table.this.name
